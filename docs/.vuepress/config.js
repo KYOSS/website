0d03316285e0529bc50e2eb7d47a9abac3e2475a
Deven Phillips <josphill@redhat.com>
2022-12-14 09:28:21 -0500

    Added ASCIInema player

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: '',
+    description: '',
+    head: [
+        ['link', {rel: "stylesheet", type: "text/css", href:"/asciinema-player/asciinema-player.css"}],
+        ['script', {src: "/asciinema-player/asciinema-player.js"}]
+    ]
+}
