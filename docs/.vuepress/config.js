2e7e3a8259fe6a258fe6b7ab6fe7a831faf8fea9
Deven Phillips <josphill@redhat.com>
2022-12-14 20:00:50 -0500

    Re-enable sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,5 +4,8 @@ module.exports = {
     head: [
         ['link', {rel: "stylesheet", type: "text/css", href:"/asciinema-player/asciinema-player.css"}],
         ['script', {src: "/asciinema-player/asciinema-player.js"}]
-    ]
+    ],
+    themeConfig: {
+        sidebar: 'auto'
+    }
 }
